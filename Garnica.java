19a14c6c8d70a80e7b642657b3ef0fd54234e622
JMartin0219 <jegarnica@paterostechnologicalcollege.edu.ph>
2025-03-11 16:56:35 +0800

    Update Garnica.java

--- Garnica.java
+++ Garnica.java
@@ -1,7 +1,7 @@
 public class Garnica {
     public static void display() {
         int age = 20; // Replace with user input if necessary
-        System.out.println("Hello! This is MARTIN contributing to the project.");
+        System.out.println("Hello! This is Garnica contributing to the project.");
         System.out.println("My current age is: " + age);
         System.out.println("Next year, I will be: " + (age + 1));
     }
